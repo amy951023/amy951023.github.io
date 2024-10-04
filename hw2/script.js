340d8d0710da0b4409431f9c82c24cc4906c77b3
amy <110422@stu.hgsh.hc.edu.tw>
2024-10-04 11:29:05 +0800

    Add files via upload

---
+++ hw2/script.js
@@ -0,0 +1,67 @@
+// 定義變數
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+let x = 0, y = 0, dx = 10, dy = 10, r = 30, color = "#84C1FF";
+let canMove=0;
+
+// 畫圓形
+function drawBall()
+{
+ctx.beginPath();
+ctx.arc(x, y, r, 0, Math.PI*2); // arc(圓心x, 圓心y, 半徑, 起始角, 結束角)
+ctx.fillStyle = color;
+ctx.fill();
+ctx.closePath();
+}
+
+// 按下按鍵時觸發
+
+document.addEventListener("mousedown", mousedownHandler);
+document.addEventListener("mouseup", mouseupHandler);
+function mousedownHandler(e)
+
+{
+	canMove=1; 
+	x=e.clientX-canvas.offsetLeft;
+    y=e.clientY-canvas.offsetTop;
+		color="#"+Math.floor(Math.random()*16777215).toString(16);
+
+
+}
+
+function mouseupHandler(e)
+{
+canMove=0;
+
+}
+
+document.addEventListener("keydown", keyDownHandler);
+function keyDownHandler(e)
+{
+
+if(e.key == "ArrowRight")        x += dx;
+else if(e.key == "ArrowLeft")    x -= dx;
+else if(e.key == "ArrowUp")      y -= dy;
+else if(e.key == "ArrowDown")    y += dy;
+}
+document.addEventListener("mousemove", mouseHandler);
+function mouseHandler(e)
+{  
+    if(canMove)
+{x=e.clientX-canvas.offsetLeft;
+y=e.clientY-canvas.offsetTop;
+}
+}
+// TODO: 滑鼠移動(mousemove)時觸發，改變位置(x, y)為滑鼠目前位置(e.clientX, e.clientY)
+// ...
+
+
+
+// 更新畫布
+function draw()
+{
+
+    drawBall();
+    requestAnimationFrame(draw);
+}
+draw();
